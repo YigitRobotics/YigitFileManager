061694dc21c25337ea7f47f0d3b0af6fa78a797b
Yiğit Ilgaz Aydoğdu <yigit@Yigit-MacBook-Air.local>
2026-04-09 15:09:02 +0300

    fileWriter added

---
+++ yigitFileWriter/headers/yigitfw_headers.h
@@ -0,0 +1,17 @@
+#ifndef YIGITF_HEADERS_H
+#define YIGITF_HEADERS_H
+
+// --- DEFINES -----
+#define BUFFER_SIZE 1024
+
+// ---- Fonksiyonlar ----
+void print_banner(FILE *banner_file);
+void get_banner();
+
+// ---- RENKLER -----
+#define CYAN "\e[0;36m"
+#define GREEN "\e[0;32m"
+#define BLUE "\e[0;34m"
+#define RESET "\e[0m"
+
+#endif
